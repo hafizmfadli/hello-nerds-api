92c7562e9988655d429876785ec8805c1d2a9560
hafizmfadli <lshafizmf1st@gmail.com>
2022-05-12 21:21:24 +0700

    migration: user table & address table

---
+++ migrations/000002_shipping_address_table.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS shipping_address (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  addresses VARCHAR(255) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  province VARCHAR(100) NOT NULL,
+  city VARCHAR(100) NOT NULL,
+  district VARCHAR(100) NOT NULL,
+  village VARCHAR(100) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_id INT,
+  CONSTRAINT fk_user
+  FOREIGN KEY (user_id)
+    REFERENCES users(id),
+  PRIMARY KEY (id)
+);
