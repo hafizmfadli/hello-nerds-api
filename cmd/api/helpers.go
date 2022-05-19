3c536c8b830d2e4ec83bcab3b38927dc343aa51b
hafizmfadli <lshafizmf1st@gmail.com>
2022-05-19 11:57:56 +0700

    add new helper function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -204,6 +204,35 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 	return i
 }
 
+// readPositiveInt read a string value from the query string and covert it to an integer and write it to dest.
+// If the value is not positive or couldn't be converted to an integer, then we record an
+// error message in the provided Validator instance.
+func (app *application) readPositiveInt (qs url.Values, key string, dest *int, v *validator.Validator) {
+		// Extract the value from the query string.
+		s := qs.Get(key)
+
+		// If no key exists (or the value is empty) then return the default value.
+		if s == "" {
+			v.AddError(key, "can't be empty")
+			return
+		}
+	
+		// Try to convert the value to an int. If this fails, add an error message to the
+		// validator instance and return the default value.
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			v.AddError(key, "must be integer value")
+			return
+		}
+
+		if i <= 0 {
+			v.AddError(key, "must be greater than zero")
+			return
+		}
+	
+		*dest = i
+}
+
 // isQueryParamExists helper read URL and check whether particular query param
 // exist or not
 func (app *application) isQueryParamExists (qs url.Values, key string) bool {
