32c3d33a6f20a213a53ae935c6807424df19c33c
Raihan <50223627+Raihan10@users.noreply.github.com>
2022-05-20 00:04:32 +0700

    add update quantity cart by id
    * create route for update quantity in a cart
    * create update quantity cart handler
    * create query for updating cart quantity

--- cmd/api/cart.go
+++ cmd/api/cart.go
@@ -69,3 +69,50 @@ func (app *application) showCartHandler(w http.ResponseWriter, r *http.Request) 
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) updateQuantityCartHandler(w http.ResponseWriter, r *http.Request) {
+
+	// create an anoymous struct to hold the expected data from the request body
+	var input struct {
+		ID       int64 `json:"id"`
+		Quantity int64 `json:"quantity"`
+	}
+
+	// get id cart from query param
+	id, err := app.readIDParam(r)
+
+	input.ID = id
+
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// parse the request body into the anonymous struct
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// copy the data from the request body into a new Cart struct
+	cart := &data.Cart{
+		ID:       input.ID,
+		Quantity: input.Quantity,
+	}
+
+	// insert cart data into the database
+	err = app.models.Carts.UpdateQuantity(cart)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Note that we also change this to send the client a 202 Accepted status code.
+	// This status code indicates that the request has been accepted for processing, but
+	// the processing has not been completed
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"cart": cart}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
